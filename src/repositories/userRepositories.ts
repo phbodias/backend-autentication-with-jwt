b0376a2f15cd47751d7134e1fb07a33edf23edb4
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-17 23:39:26 -0300

    Função para checar se já existe usuário com determinado email

--- src/repositories/userRepositories.ts
+++ src/repositories/userRepositories.ts
@@ -6,4 +6,8 @@ export async function create(data: newUser) {
     //use essa função para inserir um novo usuário em seu banco
 }
 
+export async function emailIsAvailable(email: string){
+    //Caso sua regra de negócio impeça reincidência de email, use essa função para checar se já existe no banco um cadastro refente ao email passado
+}
+
 
