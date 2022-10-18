33f817653a72cc9d244ffd8147fe5b201f158bd7
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-18 01:05:04 -0300

    Controller para logar usuário

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,9 +1,16 @@
 import { Response, Request } from "express";
 import newUser from "../types/signUpType";
 import * as userServices from "../services/userServices";
+import signInUser from "../types/signInType";
 
 export async function create(req: Request, res: Response) {
   const user: newUser = req.body;
   await userServices.create(user);
   return res.sendStatus(200);
+}
+
+export async function signIn(req: Request, res: Response) {
+  const user: signInUser = req.body;
+  const token = await userServices.signIn(user);
+  return res.status(200).send(token);
 }
