f5cbdd2a6f4fe53ea468f9006d3c3f9a0818c13f
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-18 01:12:39 -0300

    :sparkles: feat: Rota para logar usuário e retornar token

--- src/route/userRoute.ts
+++ src/route/userRoute.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { validateSchemaMiddleware } from "../middlewares/schemasMiddleware";
 import { signupSchema } from "../schemas/signUpSchemas";
 import * as userControllers from "../controllers/userControllers";
+import { signInSchema } from "../schemas/signInSchemas";
 
 const userRoute = Router();
 
@@ -9,6 +10,12 @@ userRoute.post(
   "/sign-up",
   validateSchemaMiddleware(signupSchema),
   userControllers.create
+);
+
+userRoute.post(
+  "/sign-in",
+  validateSchemaMiddleware(signInSchema),
+  userControllers.signIn
 );
 
 export default userRoute;
