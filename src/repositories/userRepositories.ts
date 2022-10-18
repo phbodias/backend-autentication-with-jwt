3b9fef06f798bd2ad52ddf7fedbdb9644f9c0e21
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-17 23:45:34 -0300

    Tipando retorno no userRepository

--- src/repositories/userRepositories.ts
+++ src/repositories/userRepositories.ts
@@ -7,12 +7,12 @@ export async function create(data: newUser) {
   return;
 }
 
-export async function emailIsAvailable(email: string) {
-  //Caso sua regra de negócio impeça reincidência de email, use essa função para checar se já existe no banco um cadastro refente ao email passado
+export async function emailIsAvailable(email: string): Promise<boolean> {
+  //Caso sua regra de negócio impeça reincidência de email, use essa função para checar se já existe no banco um cadastro refente ao email passado, retornando um booleano
   return;
 }
 
-export async function nameIsAvailable(name: string) {
-  //Caso sua regra de negócio impeça reincidência de nome de usuário, use essa função para checar se já existe no banco um cadastro de usuário com esse nome
+export async function nameIsAvailable(name: string): Promise<boolean> {
+  //Caso sua regra de negócio impeça reincidência de nome de usuário, use essa função para checar se já existe no banco um cadastro de usuário com esse nome, retornando um booleano
   return;
 }
