a557b0568e58bee640702998a22fbb572f7ef636
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-17 23:41:30 -0300

    Return nas funçoes do repository

--- src/repositories/userRepositories.ts
+++ src/repositories/userRepositories.ts
@@ -3,13 +3,16 @@
 import newUser from "../types/signUpType";
 
 export async function create(data: newUser) {
-    //use essa função para inserir um novo usuário em seu banco
+  //use essa função para inserir um novo usuário em seu banco
+  return;
 }
 
-export async function emailIsAvailable(email: string){
-    //Caso sua regra de negócio impeça reincidência de email, use essa função para checar se já existe no banco um cadastro refente ao email passado
+export async function emailIsAvailable(email: string) {
+  //Caso sua regra de negócio impeça reincidência de email, use essa função para checar se já existe no banco um cadastro refente ao email passado
+  return;
 }
 
-export async function nameIsAvailable(name: string){
-    //Caso sua regra de negócio impeça reincidência de nome de usuário, use essa função para checar se já existe no banco um cadastro de usuário com esse nome
+export async function nameIsAvailable(name: string) {
+  //Caso sua regra de negócio impeça reincidência de nome de usuário, use essa função para checar se já existe no banco um cadastro de usuário com esse nome
+  return;
 }
