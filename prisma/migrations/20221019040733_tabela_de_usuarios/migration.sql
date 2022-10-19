47a9613852761a09c674c5142e932797e78de312
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-19 01:08:16 -0300

    Criada tabela de usuários

---
+++ prisma/migrations/20221019040733_tabela_de_usuarios/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
